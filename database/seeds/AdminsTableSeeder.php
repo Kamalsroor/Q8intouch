eff1474505867c6c6ed0ec84e9bf619a5a1a7deb
kamalsroor <kamal.s.sroor@gmail.com>
2021-01-27 18:37:02 +0200

    add admin mail

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -21,6 +21,16 @@ class AdminsTableSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ]);
+
+        Admin::create([
+            'name' => "Admin Name",
+            'email' => "admin@admin.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+
         factory(App\Admin::class, 50)->create();
 
     }
