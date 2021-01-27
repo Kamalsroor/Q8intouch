6b5183480ea53b9fe18c50bf4eaa8039ce406717
kamalsroor <kamal.s.sroor@gmail.com>
2021-01-27 18:35:29 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 Route::group(['middleware' => 'auth'], function () {
 
+    Route::get('/', 'HomeController@index');
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('admin', 'AdminController');
     Route::resource('user', 'UserController');
